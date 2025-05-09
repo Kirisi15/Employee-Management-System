3e6af7ab76b9da4875ec84c15d88497db59e34c9
Kirisi15 <kkirisigan@gmail.com>
2025-05-09 23:28:19 +0530

    Get mapping created

--- src/main/java/com/example/Employee_management_system/Service/EmployeeService.java
+++ src/main/java/com/example/Employee_management_system/Service/EmployeeService.java
@@ -2,6 +2,12 @@ package com.example.Employee_management_system.Service;
 
 import com.example.Employee_management_system.DTO.EmployeeDto;
 
+import java.util.List;
+
 public interface EmployeeService {
     EmployeeDto createEmployee(EmployeeDto employeeDto);
+
+    EmployeeDto getEmployeeById(Long employeeId);
+
+    List<EmployeeDto> getAllEmployees();
 }
